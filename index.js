2c402407d1f99504bd3ce014370d878b2b18daad
RichardBoy05 <73103244+RichardBoy05@users.noreply.github.com>
2021-06-03 14:02:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
-const noswear = require("./antiswearing.js");
+import { warnsystem } from './antiswearing.js';
 
 bot.login(process.env.token); //configuration
 
@@ -14,7 +14,7 @@ bot.on("message", (message) =>{
         message.channel.send("Agli ordini!");
     }
 
-    noswear.warnsystem();
+    warnsystem();
 
 
 })
