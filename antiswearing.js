8720e62602576123b3347e0026e6cbec10c0d066
RichardBoy05 <73103244+RichardBoy05@users.noreply.github.com>
2021-06-03 13:34:07 +0200

    Create antiswearing.js

---
+++ antiswearing.js
@@ -0,0 +1,13 @@
+// this file contains the code to avoid swearing in the server
+
+const badwords = ["cazzo", "figa", "merda", "porco", "porca", "troia"];
+
+function warnsystem(){
+
+    for (var i = 0; i < badwords.length; i++){
+        if (message.content == badwords[i]){
+            message.channel.send("Evita di esprimerti in questo modo!!");
+        }
+    }
+
+}
