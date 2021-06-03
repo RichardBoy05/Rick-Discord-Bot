1c6269e237a64790375b6c9d32571b360febba37
RichardBoy05 <73103244+RichardBoy05@users.noreply.github.com>
2021-06-03 21:39:49 +0200

    Update antiswearing.js

--- antiswearing.js
+++ antiswearing.js
@@ -1,6 +1,18 @@
 // this file contains the code to avoid swearing in the server
 
-const badwords = ["cazzo", "figa", "merda", "porco", "porca", "troia"];
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+const badwords = ["cazzo", "figa", "merda", "porco", "porca", "troia", "puttana"];
 
 
-module.exports = { badwords };
+function warnsystem(){
+
+for (var i = 0; i < badwords.length; i++){ //check bad words in messages
+    if (message.content == badwords[i]){
+        message.channel.send("Evita di esprimerti in questo modo!! "+ message.author.toString());
+    }
+}
+}
+
+
+module.exports = { warnsystem };
