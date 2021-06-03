db6289092449914c34de12748c46000ad1c0662f
RichardBoy05 <73103244+RichardBoy05@users.noreply.github.com>
2021-06-03 22:32:48 +0200

    Update antiswearing.js

--- antiswearing.js
+++ antiswearing.js
@@ -1,6 +1,6 @@
 // this file contains the code to avoid swearing in the server
 
-const badwords = ["cazzo", "figa", "merda", "porco", "porca", "troia", "puttana"];
+const badwords = ["cazzo", "figa", "fica", "merda", "porco", "porca", "troia", "puttana", "bastardo", "bastarda", "zoccola", "negro", "coglione", "ricchione", "minchia", "mignotta", "frocio", "finocchio", "ditalino", "stronzo", "stronza", "ano", "vagina"];
 
 function checkText(message){
 
