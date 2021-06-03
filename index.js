338904b1d7d096da43a0aa2f57dc81faed0f593f
RichardBoy05 <73103244+RichardBoy05@users.noreply.github.com>
2021-06-03 14:21:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const bot = new Discord.Client();
-const noswear = require('./antiswearing.js');
+const noswear = require("./antiswearing.js");
 
 bot.login(process.env.token); //configuration
 
