2222adca0ae96e1176821e235b13b81925308dcb
RichardBoy05 <73103244+RichardBoy05@users.noreply.github.com>
2021-06-03 11:51:33 +0200

    Rick Discord Bot
    Coding my personal Discord Bot

---
+++ index.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+
+var token = 'ODQ5OTQxMDI2MzQ4NzI4MzUw.YLifQw.EujYOv5mRfJa7zdwZAwo_v_leMk';
+
+bot.login(process.env.token); //configuration
+
+//here starts the code
+
+bot.on("message", (message) =>{
+
+    if (message.content == '!rick'){
+        message.channel.send("Presente!");
+    }
+
+})
+
+
