034f96e21f58f6c3a7ab7ff3b408ef348d6ec208
RichardBoy05 <73103244+RichardBoy05@users.noreply.github.com>
2021-06-04 14:33:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,14 +16,24 @@ bot.on("message", (message) =>{
     }
 
 
-    if ((noswear.checkText(message.content.toLowerCase()) == true) && message.channel.name != "warnings-bans"){
-        var warning_channel_id = bot.channels.cache.get("850329118914641951");
-        var options = {'weekday': 'long', 'month': '2-digit', 'day': '2-digit'};
-        var date = new Date();
-        var dateString = date.toLocaleString('it-IT', options)+ "/" + date.getFullYear() + " alle ore " + (date.getHours()<10?'0':'') + date.getHours() + ":" + (date.getMinutes()<10?'0':'') + date.getMinutes();
-        message.delete();
-        message.channel.send("La volgarità non è ammessa nel server!"+ message.author.toString());
-        warning_channel_id.send(""+ message.author.toString()+" WARNING! L'utilizzo di termini volgari nel server può portarti a venir mutato o bannato.\n Messaggio incriminato: '"+
+    if ((noswear.checkText(message.content.toLowerCase()) == true) && message.channel.name != "warnings-bans"){ //check the content of the message and compare it to a list
+                                                                                                                // of bad words 
+        
+        var warning_channel_id = bot.channels.cache.get("850329118914641951"); // id of the warning-bans channel
+
+        
+        
+        var options = {'weekday': 'long', 'month': '2-digit', 'day': '2-digit'}; //defines the structure of the date
+        var date = new Date(); //gets the date
+        var dateString = date.toLocaleString('it-IT', options)+ "/" + date.getFullYear() + " alle ore " +
+        +(date.getHours()<10?'0':'') + date.getHours() + ":" + (date.getMinutes()<10?'0':'') + date.getMinutes(); //builds the string of the date
+        
+        
+        
+        message.delete(); //delete the bad message
+        message.channel.send("La volgarità non è ammessa nel server!"+ message.author.toString()); //replaces the bad message
+        warning_channel_id.send(""+ message.author.toString()+" WARNING! L'utilizzo di termini volgari nel server può portarti"+ //sends the warning to the warnings-bans channel
+        " a venir mutato o bannato.\n Messaggio incriminato: '"+
         message.content+"';\n Canale: '"+
         message.channel.name+"';\n Data: "+
         dateString+".");
