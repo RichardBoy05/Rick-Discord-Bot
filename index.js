e5812c8ce265925e6e5638c1975b9f6af80564a9
RichardBoy05 <73103244+RichardBoy05@users.noreply.github.com>
2021-06-04 13:42:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,12 +18,13 @@ bot.on("message", (message) =>{
 
     if ((noswear.checkText(message.content.toLowerCase()) == true) && message.channel.name != "warnings-bans"){
         var warning_channel_id = bot.channels.cache.get("850329118914641951");
+        var date = new Date();
         message.delete();
         message.channel.send("La volgarità non è ammessa nel server!"+ message.author.toString());
         warning_channel_id.send(""+ message.author.toString()+" WARNING! L'utilizzo di termini volgari nel server può portati a venir mutato o bannato.\n Messaggio incriminato: '"+
         message.content+"';\n Canale: '"+
         message.channel.name+"';\n Data: "+
-        ".");
+        date+".");
         
 
 
