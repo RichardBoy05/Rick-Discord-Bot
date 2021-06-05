a6dfaa9cc1642461667e31504e196de05254b99e
RichardBoy05 <73103244+RichardBoy05@users.noreply.github.com>
2021-06-05 19:42:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,28 +18,6 @@ bot.on("message", (message) =>{
 
     clear.clearChat(message); // command to remove a certain amount of messages in a channel (r/clear)
 
-    if (message.content == "r/ciao"){ // checks if the bot is online or not
-        message.channel.send("**Regole per una buona convivenza:** \n\n"
-       + "- No al linguaggio scurrile o aggressivo.\n"
-       + "- Non siate sessisti, razzisti, volgari.\n"
-       + "- Meglio non parlare di politica o religione.\n"
-       + "- Sì alle discussioni animate, ma non litigate.\n"
-       + "- Non parlate di attività illegali e non invitate all'investimento.\n"
-       + "- Usate sempre il canale giusto per l'argomento di cui volete discutere.\n"
-       + "- Rispettate i moderatori e gli amministratori.\n"
-       + "- Non inserite links nei canali dedicati alle discussioni e non iniziate discussioni nei canali dedicati ai links.\n"
-       + "- Evitate di spezzettare un messaggio.\n"
-       + "- Non scrivete in maiuscolo, solo gli amministratori o i moderatori se necessario possono farlo.\n"
-       + "- Non iniziate una nuova discussione se non si è conclusa la precedente.\n"
-       + "- Nelle chat vocali non sovrapponete la vostra voce a quella degli altri, attendete che un utente concluda l'espressione di un concetto prima di intervenire.\n"
-       + "- Siate gentili e cordiali con i nuovi entrati.\n"
-       + "- Rispondete alle richieste di aiuto in chat.\n\n"
-        
-        +"_Buona permanenza!_  ~ **[OWNER] JustAPuzwick**")
-        .then(msg => msg.react("✅"));   
-    }
-
-
 })
 
 
@@ -53,4 +31,15 @@ bot.on("guildMemberAdd", (member) =>{ // triggers when someone joins the server
 
 bot.on("guildMemberRemove", (member) =>{ // triggers when someone leaves the server
     bot.channels.cache.get("849212228716593152").send(member.toString()+" ha deciso di abbandonarci 😢 ...Che loser!");
+})
+
+
+
+bot.on("messageReactionAdd", async function(messageReaction, user){
+
+    if(user.bot) return
+    if (messageReaction.member.roles.cache.has("849207774541250590")){
+        message.channel.send("Error but gggs");
+    }
+
 })
