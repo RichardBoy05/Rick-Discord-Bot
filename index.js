edd7d9e1d11ce48ab0278f569420cecf786459b7
RichardBoy05 <73103244+RichardBoy05@users.noreply.github.com>
2021-06-05 18:52:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -23,13 +23,13 @@ bot.on("message", (message) =>{
 
 
 
-bot.on("guildMemberAdd", (member) =>{
+bot.on("guildMemberAdd", (member) =>{ // triggers when someone joins the server
     bot.channels.cache.get("849212228716593152").send("Benvenuto nel server " + member.toString() +
     "\:smile: . Ti auguriamo una buona permanenza! Sei il membro numero " + member.guild.memberCount + "! \:heart:");
 })
 
 
 
-bot.on("guildMemberRemove", (member) =>{
+bot.on("guildMemberRemove", (member) =>{ // triggers when someone leaves the server
     bot.channels.cache.get("849212228716593152").send(member.toString()+" ha deciso di abbandonarci 😢 ...Che loser!");
 })
