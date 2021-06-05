af62e7a43b926541c97634d6f63165cec79863a3
RichardBoy05 <73103244+RichardBoy05@users.noreply.github.com>
2021-06-05 15:29:33 +0200

    Update antiswearing.js

--- antiswearing/antiswearing.js
+++ antiswearing/antiswearing.js
@@ -43,4 +43,37 @@ function countWords(revtext){ // returns an array of the different words of the 
   }
 
 
-module.exports = { checkText }; // exports the checkText function so it will be usable outside this file
+  function warnsystem(data, bot){
+
+    if ((checkText(data.content.toLowerCase()) == true) && data.channel.name != "warnings-bans"){ //check the content of the message and compare it to a list
+        // of bad words 
+
+    var warning_channel_id = bot.channels.cache.get("850329118914641951"); // id of the warning-bans channel
+
+
+
+    var options = {'weekday': 'long', 'month': '2-digit', 'day': '2-digit'}; //defines the structure of the date
+    var date = new Date(); //gets the date
+    var dateString = date.toLocaleString('it-IT', options)+ "/" + date.getFullYear() + " alle ore " +
+    + date.getHours() + ":" + (date.getMinutes()<10?"0":"") + date.getMinutes(); //builds the string of the date
+
+
+
+    data.delete(); //delete the bad message
+    data.channel.send("La volgarità non è ammessa nel server!"+ data.author.toString()); //replaces the bad message
+    warning_channel_id.send(data.author.toString()+" WARNING! L'utilizzo di termini volgari nel server può portarti"+ //sends the warning to the warnings-bans channel
+    " a venir mutato o bannato.\n Messaggio incriminato: '"+
+    data.content+"';\n Canale: '"+
+    data.channel.name+"';\n Data: "+
+    dateString+".");
+
+
+    }
+
+}
+
+
+module.exports = { warnsystem };
+
+
+     
