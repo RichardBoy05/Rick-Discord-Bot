202dc91064d8061b54fb97620538e6da849a690c
RichardBoy05 <73103244+RichardBoy05@users.noreply.github.com>
2021-06-16 13:20:49 +0200

    Update clear.js

--- clear/clear.js
+++ clear/clear.js
@@ -1,30 +1,31 @@
 
-function clearChat(data){
-    
-    if(data.content.startsWith("r/clear")){
-        if(!(data.member.roles.cache.has("849207774541250590"))){
+function clearChat(data) {
+
+    if (data.content.startsWith("r/clear")) {
+        if (!(data.member.roles.cache.has("849207774541250590"))) {
             data.channel.send("Non hai il permesso di eseguire questo comando!");
             return
         }
 
         var count = data.content.slice(8);
         count = parseInt(count);
+        count --
 
-        if(!count){
+        if (!count) {
             data.channel.send("Inserisci un numero valido!");
             return
         }
 
-        if(count > 100){
+        if (count > 100) {
             data.channel.send("Puoi eliminare al massimo 100 messaggi alla volta!");
             return
         }
 
         data.channel.bulkDelete(count, true);
         data.channel.send("Ho eliminato " + count + " messaggi!")
-        .then(msg =>{
-            msg.delete({timeout:3000})
-        })
+            .then(msg => {
+                msg.delete({ timeout: 3000 })
+            })
 
     }
 
