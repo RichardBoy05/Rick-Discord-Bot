686a25bd2681c1a494125441803f2183226b4a75
RichardBoy05 <73103244+RichardBoy05@users.noreply.github.com>
2021-06-16 13:20:44 +0200

    Create rules.js

---
+++ rules/rules.js
@@ -0,0 +1,30 @@
+
+function addedReaction(user, messageReaction) {
+
+    if (user.bot) return
+    if (messageReaction.message.partial) await messageReaction.message.fetch();
+
+    if (messageReaction.message.id == ("852199373667303494")) {
+
+        var member = messageReaction.message.guild.members.cache.find(x => x.id == user.id);
+        member.roles.add("850718041901105163");
+    }
+}
+
+
+
+function removedReaction(user, messageReaction) {
+
+    if (user.bot) return
+    if (messageReaction.message.partial) await messageReaction.message.fetch();
+
+    if (messageReaction.message.id == ("852199373667303494")) {
+
+        var member = messageReaction.message.guild.members.cache.find(x => x.id == user.id);
+        member.roles.remove("850718041901105163");
+
+    }
+}
+
+
+module.exports = {addedReaction, removedReaction}
