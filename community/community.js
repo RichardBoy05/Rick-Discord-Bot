5f63c6d73014c48da8b965fb7d948037c143b43a
RichardBoy05 <73103244+RichardBoy05@users.noreply.github.com>
2021-06-18 17:23:22 +0200

    Create community.js

---
+++ community/community.js
@@ -0,0 +1,52 @@
+const Discord = require("discord.js");
+const prefix = "//";
+const channel_id = "851438938044104724";
+
+
+function suggOrChall(message, client) {
+
+    if (message.content.startsWith(prefix + "suggest") && message.channel.id == channel_id) {
+
+        var suggestion = message.content.slice(10);
+        if (suggestion == "") {
+            client.channels.cache.get(channel_id).send("Inserisci un suggerimento valido!");
+            return
+        }
+
+        createEmbed(message, suggestion, true);
+        message.delete();
+
+        // suggestions
+
+        if (message.content.startsWith(prefix + "challenge") && message.channel.id == channel_id) {
+
+            var challenge = message.content.slice(12);
+            if (challenge == "") { }
+            client.channels.cache.get(channel_id).send("Inserisci una challenge valida!");
+            return
+
+        }
+
+        createEmbed(message, challenge, false);
+        message.delete();
+
+        //challenges
+
+    }
+}
+
+
+
+function createEmbed(raw, data, isSugg) {
+
+    var embed = new Discord.MessageEmbed() // creating the embed message
+    .setTitle("💡 Suggerimento di " + raw.author.toString());
+    isSugg == true ? embed.setColor("#07D3F8") : embed.setColor("#F87807");
+
+}
+
+
+
+
+
+module.exports = { suggOrChall };
