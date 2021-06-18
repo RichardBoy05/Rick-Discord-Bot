245c67a77b366e1d62d1dbb3eb2a05850814771d
RichardBoy05 <73103244+RichardBoy05@users.noreply.github.com>
2021-06-18 10:42:44 +0200

    Update suggestions.js

--- suggestions/suggestions.js
+++ suggestions/suggestions.js
@@ -9,11 +9,16 @@ function makeSuggestion(message, client) {
     if (message.content.startsWith(prefix + "suggest") && message.channel.id == makesugg_id){
 
         var suggestion = message.content.slice(10);
+
+        if (suggestion == ""){
+            client.channels.cache.get(makesugg_id).send("Inserisci un suggerimento valido!");
+        }
+
         var embed = new Discord.MessageEmbed()
         .setColor("#0000ff")
-        .setTitle("Suggerimento")
-        .setAuthor("Richard")
-        .setDescription("Sperem");
+        .setTitle("💡 Suggestions by " + message.member.user.username)
+        .setDescription(suggestion)
+        .setThumbnail(message.member.user.avatarURL({ dynamic: true }))
 
         client.channels.cache.get(allsugg_id).send(embed);
 
