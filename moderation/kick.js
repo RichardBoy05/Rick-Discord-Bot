464ab71b2b1d61df99d31c18b6a43eefcc452a6d
RichardBoy05 <73103244+RichardBoy05@users.noreply.github.com>
2021-06-20 18:06:45 +0200

    Update kick.js

---
+++ moderation/kick.js
@@ -0,0 +1,30 @@
+const mod_channel_id = "850329118914641951";
+const prefix = "//";
+
+function kickuser(message){
+
+    if (message.content.startsWith(prefix + "kick")){
+
+        var user = message.mentions.members.first();
+
+        if (!message.member.hasPermission("KICK_MEMBERS")){
+            message.channel.send("Non hai il permesso di esguire questo comando!");
+            return;
+        }
+
+        if (!user){
+            message.channel.send("Devi menzionare un utente per espellerlo!");
+            return;
+        }
+
+        if(!user.kickable){
+            message.channel.send("Il bot non ha il permesso di espellere questo utente!");
+        }
+
+        user.kick();
+        message.channel.send("Comando eseguito!");
+
+
+
+    }
+}
