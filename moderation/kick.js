07996a52727d955505d52ad357bd74454b9340be
RichardBoy05 <73103244+RichardBoy05@users.noreply.github.com>
2021-06-20 18:19:22 +0200

    Update kick.js

--- moderation/kick.js
+++ moderation/kick.js
@@ -1,11 +1,18 @@
 const mod_channel_id = "850329118914641951";
 const prefix = "//";
+const split = ":";
 
 function kickUser(message){
 
     if (message.content.startsWith(prefix + "kick")){
 
         var user = message.mentions.members.first();
+        var reason = message.substring(message.indexOf(split) + 1);
+
+        if (reason == ""){
+            message.channel.send("Aggiungi il motivo del kick!");
+            return;
+        }
 
         if (!message.member.hasPermission("KICK_MEMBERS")){
             message.channel.send("Non hai il permesso di esguire questo comando!");
@@ -23,6 +30,9 @@ function kickUser(message){
 
         user.kick();
         message.channel.send("Comando eseguito!");
+        mod_channel_id.send("L'utente " + user + " è stato kickato da " + message.author.toString() + "con la seguente motivazione: " + reason);
+
+
 
 
 
